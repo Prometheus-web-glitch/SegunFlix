f1a19d9f6dc076af634b1e3e6324796256b0c8bb
Olusegun Olutade <segun.olutade@gmail.com>
2022-02-16 14:55:21 -0600

    Added the collection view

---
+++ SegunFlix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  SegunFlix
+//
+//  Created by Students on 2/15/22.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
